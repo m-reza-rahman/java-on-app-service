244ee794c77ed148f9486959a280eb17caf35cfd
Reza Rahman <reza_rahman@mail.com>
2024-12-26 16:06:17 -0500

    Minor cleanup and adapting to App Service

--- jakartaee/src/main/java/com/azure/samples/todo/JaxRsActivator.java
+++ jakartaee/src/main/java/com/azure/samples/todo/JaxRsActivator.java
@@ -4,6 +4,4 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/resources")
-public class JaxRsActivator extends Application {
-
-}
+public class JaxRsActivator extends Application {}
