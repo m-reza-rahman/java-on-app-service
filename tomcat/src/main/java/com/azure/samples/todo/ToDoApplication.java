b713b7ce90b2801252d2191c6b00a14f98b9f6d2
galiacheng <haixia.cheng@microsoft.com>
2024-12-31 15:40:23 +0800

    Spring data jpa starter has enabled transaction manager.

--- tomcat/src/main/java/com/azure/samples/todo/ToDoApplication.java
+++ tomcat/src/main/java/com/azure/samples/todo/ToDoApplication.java
@@ -3,10 +3,8 @@ package com.azure.samples.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class ToDoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
