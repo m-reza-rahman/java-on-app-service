3a7d2e445d831ff7c6bc5d558f1d70ef26e86135
galiacheng <haixia.cheng@microsoft.com>
2025-01-03 18:24:00 +0800

    Handle MethodArgumentNotValidException.

--- tomcat/src/main/java/com/azure/samples/todo/GlobalExceptionHandler.java
+++ tomcat/src/main/java/com/azure/samples/todo/GlobalExceptionHandler.java
@@ -2,10 +2,14 @@ package com.azure.samples.todo;
 
 import jakarta.validation.ConstraintViolationException;
 
+import java.util.logging.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -13,13 +17,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@ControllerAdvice
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
+
     @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, List<String>>> handleValidationErrors(ConstraintViolationException ex) {
+        logger.warning("ConstraintViolationException error occurred: " + ex.getMessage());
         List<String> errors = ex.getConstraintViolations().stream()
                 .map(violation -> violation.getMessage())
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, List<String>>> handleArgumentErrors(MethodArgumentNotValidException ex) {
+        logger.warning("MethodArgumentNotValidException error occurred: " + ex.getMessage());
+        List<String> errors = ex.getAllErrors().stream()
+                .map(violation -> violation.getDefaultMessage())
                 .collect(Collectors.toList());
         return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
